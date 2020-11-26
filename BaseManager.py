e8572a72be98d015d64d7607e96d1546b58a6aa2
Vinicius Dantas <vdj@cin.ufpe.br>
2020-11-26 17:00:27 -0300

    distribuição de workers e correção de bugs

--- BaseManager.py
+++ BaseManager.py
@@ -1,23 +1,28 @@
 import sc2
 import Agent
 import math
+import random
 from sc2.constants import COMMANDCENTER, SCV
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
 from Agent import Agent
 
 class BaseManager(Agent):
-    def buildHandler(self, task):
-        chosen = self.env.select_build_worker(task.location)
-        if chosen and self.env.can_afford(task.id) and self.env.already_pending(task.id) == 0:
-            result = self.env.do(chosen.build(task.id, task.location))
-            if result:
-                return True
+    async def buildHandler(self, task):
+        if self.env.can_afford(task.id) and self.env.already_pending(task.id) == 0:
+            if task.location == None:
+                task.location = await self.env.find_placement(task.id,random.choice(self.env.structures).position, 100, True, 3, True)
+            if task.location:
+                chosen = self.env.workers.closest_to(task.location)
+                if chosen:
+                    result = self.env.do(chosen.build(task.id, task.location))
+                    if result:
+                        return True
         return False
 
-    def expansionHandler(self, task):
+    async def expansionHandler(self, task):
         for structure in self.env.structures(task.id).ready.idle:
-            if self.env.can_afford(task.upgrade):
+            if not structure.has_add_on and self.env.can_afford(task.upgrade):
                 if structure.is_active:
                     self.env.do(structure.stop())
                 result = self.env.do(structure.build(task.upgrade))
@@ -25,7 +30,7 @@ class BaseManager(Agent):
                     return True
         return False
 
-    def trainHandler(self,task):
+    async def trainHandler(self,task):
         for structure in self.env.structures(task.location).ready.idle:
             if self.env.can_afford(task.id):
                 result = self.env.do(structure.train(task.id))
@@ -33,7 +38,7 @@ class BaseManager(Agent):
                     return True
         return False
 
-    def abilityHandler(self, task):
+    async def abilityHandler(self, task):
         for st in self.env.structures(task.id).ready.idle:
             if st.is_active:
                 self.env.do(st.stop())
@@ -43,7 +48,7 @@ class BaseManager(Agent):
                     return True
         return False
 
-    def researchHandler(self, task):
+    async def researchHandler(self, task):
         for structure in self.env.structures(task.id).ready.idle:
             if self.env.can_afford(task.upgrade):
                 if structure.is_active:
@@ -53,26 +58,28 @@ class BaseManager(Agent):
                     return True
         return False
 
-    def handleTasks(self):
+    async def handleTasks(self):
         for i in range(len(self.tasks)):
-            result = self.handler[self.tasks[i].type](self, self.tasks[i])
+            result = await self.handler[self.tasks[i].type](self, self.tasks[i])
             if (result):
                 self.tasks.pop(i)
                 break
 
     async def doAction(self):
-        self.handleTasks()
+        await self.handleTasks()
         self.buildWorkers()
-        self.distributeWorkers()
+        self.distributeIdleWorkers()
+        await self.env.distribute_workers()
 
     def buildWorkers(self):
         for commandCenter in self.env.townhalls.idle:
             if self.env.can_afford(SCV) and self.env.supply_left > 0 and self.env.supply_workers < self.maxWorkers:
                 self.env.do(commandCenter.train(SCV))
 
-    def distributeWorkers(self):
+    def distributeIdleWorkers(self):
         for scv in self.env.workers.idle:
             self.env.do(scv.gather(self.env.mineral_field.closest_to(scv)))
 
-    maxWorkers = 36
+    maxWorkers = 40
+    focusGas = True
     handler = [buildHandler, trainHandler, abilityHandler, researchHandler, expansionHandler]
--- Strategist.py
+++ Strategist.py
@@ -7,6 +7,7 @@ from BaseManager import BaseManager
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
 from sc2.ids.upgrade_id import UpgradeId
+from sc2.units import Units
 from Task import Task
 
 class Strategist(sc2.BotAI) :
@@ -20,13 +21,10 @@ class Strategist(sc2.BotAI) :
             check intel and determine changes for current buildOrder
         """
 
-    async def sendTask(self) :
+    def sendTask(self) :
         tf = self.time_formatted
         while self.buildOrder != [] and tf >= self.buildOrder[0][0]:
             order = self.buildOrder.pop(0)
-            if order[1].type == 0:
-                if order[1].location == None:
-                    order[1].location = await self.find_placement(order[1].id,random.choice(self.structures).position, 50, True, 2, True)
             self.baseManager.receiveTask(order[1])
 
     async def setArmyStance(self) :
@@ -46,6 +44,10 @@ class Strategist(sc2.BotAI) :
 
         vgs2 = self.vespene_geyser.closer_than(20, ex1)
 
+        print(vgs1)
+
+        print(vgs2)
+
         ex2 = await self.get_next_expansion()
 
         vgs3 = self.vespene_geyser.closer_than(20, ex2)
@@ -55,9 +57,8 @@ class Strategist(sc2.BotAI) :
             ("00:29",Task(0, UnitTypeId.REFINERY, vgs1[0], None)),
             ("00:44",Task(0, UnitTypeId.BARRACKS, self.main_base_ramp.barracks_in_middle, None)),
             ("01:32",Task(2, UnitTypeId.COMMANDCENTER, UnitTypeId.ORBITALCOMMAND, AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND)),
-            ("01:43",Task(0, UnitTypeId.COMMANDCENTER, ex1, None)),
             ("01:34",Task(1, UnitTypeId.REAPER, UnitTypeId.BARRACKS, None)),
-            ("01:43",Task(0, UnitTypeId.COMMANDCENTER, None, None)),
+            ("01:43",Task(0, UnitTypeId.COMMANDCENTER, ex1, None)),
             ("02:08",Task(0, UnitTypeId.FACTORY, None, None)),
             ("02:15",Task(0, UnitTypeId.SUPPLYDEPOT, None, None)),
             ("02:21",Task(0, UnitTypeId.REFINERY, vgs1[1], None)),
@@ -88,7 +89,7 @@ class Strategist(sc2.BotAI) :
             ("05:28",Task(0, UnitTypeId.FACTORY, None, None)),
             ("05:29",Task(1, UnitTypeId.SIEGETANK, UnitTypeId.FACTORY, None)),
             ("05:35",Task(0, UnitTypeId.FACTORY, None, None)),
-            ("04:41",Task(1, UnitTypeId.HELLION, UnitTypeId.FACTORY, None)),
+            ("05:45",Task(1, UnitTypeId.HELLION, UnitTypeId.FACTORY, None)),
             ("05:48",Task(2, UnitTypeId.COMMANDCENTER, UnitTypeId.ORBITALCOMMAND, AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND)),
             # ("05:58",Task(4, UnitTypeId.BARRACKS, None, UnitTypeId.TECHLAB)),
             ("06:05",Task(1, UnitTypeId.SIEGETANK, UnitTypeId.FACTORY, None)),
@@ -106,7 +107,7 @@ class Strategist(sc2.BotAI) :
 
     async def on_step(self, iteration) :
         # await self.changeBuild()
-        await self.sendTask()
+        self.sendTask()
         # await self.setArmyStance()
         await self.baseManager.doAction()
         # await self.armyLeader.doAction()
@@ -115,3 +116,118 @@ class Strategist(sc2.BotAI) :
         self.unit_command_uses_self_do = True
         self.baseManager = BaseManager(self)
         # self.armyLeader = ArmyLeader(self)
+
+    async def distribute_workers(self, performanceHeavy=True, onlySaturateGas=False):
+        mineralTags = [x.tag for x in self.mineral_field]
+        gas_buildingTags = [x.tag for x in self.gas_buildings]
+
+        workerPool = Units([], self)
+        workerPoolTags = set()
+
+        # Find all gas_buildings that have surplus or deficit
+        deficit_gas_buildings = {}
+        surplusgas_buildings = {}
+        for g in self.gas_buildings.filter(lambda x: x.vespene_contents > 0):
+            # Only loop over gas_buildings that have still gas in them
+            deficit = g.ideal_harvesters - g.assigned_harvesters
+            if deficit > 0:
+                deficit_gas_buildings[g.tag] = {"unit": g, "deficit": deficit}
+            elif deficit < 0:
+                surplusWorkers = self.workers.closer_than(10, g).filter(
+                    lambda w: w not in workerPoolTags
+                    and len(w.orders) == 1
+                    and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER]
+                    and w.orders[0].target in gas_buildingTags
+                )
+                for i in range(-deficit):
+                    if surplusWorkers.amount > 0:
+                        w = surplusWorkers.pop()
+                        workerPool.append(w)
+                        workerPoolTags.add(w.tag)
+                surplusgas_buildings[g.tag] = {"unit": g, "deficit": deficit}
+
+        # Find all townhalls that have surplus or deficit
+        deficitTownhalls = {}
+        surplusTownhalls = {}
+        if not onlySaturateGas:
+            for th in self.townhalls:
+                deficit = th.ideal_harvesters - th.assigned_harvesters
+                if deficit > 0:
+                    deficitTownhalls[th.tag] = {"unit": th, "deficit": deficit}
+                elif deficit < 0:
+                    surplusWorkers = self.workers.closer_than(10, th).filter(
+                        lambda w: w.tag not in workerPoolTags
+                        and len(w.orders) == 1
+                        and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER]
+                        and w.orders[0].target in mineralTags
+                    )
+                    # workerPool.extend(surplusWorkers)
+                    for i in range(-deficit):
+                        if surplusWorkers.amount > 0:
+                            w = surplusWorkers.pop()
+                            workerPool.append(w)
+                            workerPoolTags.add(w.tag)
+                    surplusTownhalls[th.tag] = {"unit": th, "deficit": deficit}
+
+            if all(
+                [
+                    len(deficit_gas_buildings) == 0,
+                    len(surplusgas_buildings) == 0,
+                    len(surplusTownhalls) == 0 or deficitTownhalls == 0,
+                ]
+            ):
+                # Cancel early if there is nothing to balance
+                return
+
+        # Check if deficit in gas less or equal than what we have in surplus, else grab some more workers from surplus bases
+        deficitGasCount = sum(
+            gasInfo["deficit"] for gasTag, gasInfo in deficit_gas_buildings.items() if gasInfo["deficit"] > 0
+        )
+        surplusCount = sum(
+            -gasInfo["deficit"] for gasTag, gasInfo in surplusgas_buildings.items() if gasInfo["deficit"] < 0
+        )
+        surplusCount += sum(-thInfo["deficit"] for thTag, thInfo in surplusTownhalls.items() if thInfo["deficit"] < 0)
+
+        if deficitGasCount - surplusCount > 0:
+            # Grab workers near the gas who are mining minerals
+            for gTag, gInfo in deficit_gas_buildings.items():
+                if workerPool.amount >= deficitGasCount:
+                    break
+                workersNearGas = self.workers.closer_than(10, gInfo["unit"]).filter(
+                    lambda w: w.tag not in workerPoolTags
+                    and len(w.orders) == 1
+                    and w.orders[0].ability.id in [AbilityId.HARVEST_GATHER]
+                    and w.orders[0].target in mineralTags
+                )
+                while workersNearGas.amount > 0 and workerPool.amount < deficitGasCount:
+                    w = workersNearGas.pop()
+                    workerPool.append(w)
+                    workerPoolTags.add(w.tag)
+
+        # Now we should have enough workers in the pool to saturate all gases, and if there are workers left over, make them mine at townhalls that have mineral workers deficit
+        for gTag, gInfo in deficit_gas_buildings.items():
+            if performanceHeavy:
+                # Sort furthest away to closest (as the pop() function will take the last element)
+                workerPool.sort(key=lambda x: x.distance_to(gInfo["unit"]), reverse=True)
+            for i in range(gInfo["deficit"]):
+                if workerPool.amount > 0:
+                    w = workerPool.pop()
+                    if len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_RETURN]:
+                        self.do(w.gather(gInfo["unit"], queue=True))
+                    else:
+                        self.do(w.gather(gInfo["unit"]))
+
+        if not onlySaturateGas:
+            # If we now have left over workers, make them mine at bases with deficit in mineral workers
+            for thTag, thInfo in deficitTownhalls.items():
+                if performanceHeavy:
+                    # Sort furthest away to closest (as the pop() function will take the last element)
+                    workerPool.sort(key=lambda x: x.distance_to(thInfo["unit"]), reverse=True)
+                for i in range(thInfo["deficit"]):
+                    if workerPool.amount > 0:
+                        w = workerPool.pop()
+                        mf = self.mineral_field.closer_than(10, thInfo["unit"]).closest_to(w)
+                        if len(w.orders) == 1 and w.orders[0].ability.id in [AbilityId.HARVEST_RETURN]:
+                            self.do(w.gather(mf, queue=True))
+                        else:
+                            self.do(w.gather(mf))
