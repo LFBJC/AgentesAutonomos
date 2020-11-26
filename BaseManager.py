cf4a8b6101d3481b00750a3288e000e63828ddaf
Vinicius Dantas <vdj@cin.ufpe.br>
2020-11-25 22:15:29 -0300

    correção de bugs em baseManager e adequação com versão atual do python-sc2

--- BaseManager.py
+++ BaseManager.py
@@ -7,53 +7,57 @@ from sc2.ids.ability_id import AbilityId
 from Agent import Agent
 
 class BaseManager(Agent):
-    async def buildHandler(self, task):
+    def buildHandler(self, task):
         chosen = self.env.select_build_worker(task.location)
         if chosen:
-            result = None
-            try:
-                result = await self.env.do(chosen.build(task.id, task.location))
-            except:
-                return False
-            if result != sc2.data.ActionResult.Error:
+            result = self.env.do(chosen.build(task.id, task.location))
+            if result:
                 return True
-            return False
         return False
 
-    async def trainHandler(self,task):
-        for structure in self.env.units(task.location).ready.idle:
+    def trainHandler(self,task):
+        for structure in self.env.structures(task.location).ready.idle:
             if self.env.can_afford(task.id):
-                try:
-                    return await self.env.do(structure.train(task.id))
-                except:
-                    return False
-                return True
+                result = self.env.do(structure.train(task.id))
+                if result:
+                    return True
         return False
 
-    async def upgradeHandler(self, task): # TODO: fix method exception
-        for structure in self.env.units(task.id).ready:
+    def upgradeHandler(self, task):
+        for st in self.env.structures(task.id).ready.idle:
+            if self.env.can_afford(task.upgrade) and self.env.tech_requirement_progress(task.location) == 1:
+                result = self.env.do(st(task.upgrade))
+                if result:
+                    return True
+        return False
+
+    def researchHandler(self, task):
+        for structure in self.env.structures(task.id).ready:
             if self.env.can_afford(task.upgrade):
-                try:
-                    return self.env.do(structure.research(task.upgrade))
-                except:
-                    return False
-                return True
+                result = self.env.do(structure.research(task.upgrade))
+                if result:
+                    return True
         return False
 
-    async def handleTasks(self):
+    def handleTasks(self):
         for i in range(len(self.tasks)):
-            result = await self.handler[self.tasks[i].type](self, self.tasks[i])
+            result = self.handler[self.tasks[i].type](self, self.tasks[i])
             if (result):
                 self.tasks.pop(i)
+                break
 
     async def doAction(self):
-        await self.env.distribute_workers()
-        await self.buildWorkers()
-        await self.handleTasks()
+        self.distributeWorkers()
+        self.buildWorkers()
+        self.handleTasks()
 
-    async def buildWorkers(self):
-        for commandCenter in self.env.units(COMMANDCENTER).ready.idle:
-            if self.env.can_afford(SCV):
-                await self.env.do(commandCenter.train(SCV))
+    def buildWorkers(self):
+        for commandCenter in self.env.townhalls.idle:
+            if self.env.can_afford(SCV) and self.env.supply_left > 10:
+                self.env.do(commandCenter.train(SCV))
 
-    handler = [buildHandler, trainHandler, upgradeHandler]
+    def distributeWorkers(self):
+        for scv in self.env.workers.idle:
+            self.env.do(scv.gather(self.env.mineral_field.closest_to(scv)))
+
+    handler = [buildHandler, trainHandler, upgradeHandler, researchHandler]
--- Strategist.py
+++ Strategist.py
@@ -5,6 +5,7 @@ from sc2.player import Bot, Computer
 from BaseManager import BaseManager
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
+from sc2.ids.upgrade_id import UpgradeId
 from Task import Task
 
 class Strategist(sc2.BotAI) :
@@ -29,33 +30,34 @@ class Strategist(sc2.BotAI) :
             check intel and determine best army behaviour
         """
 
-    def on_start(self) :
+    async def on_start(self) :
         # TODO: checks for building placement
         vgs = None
 
         for th in self.townhalls.ready:
-            vgs = self.state.vespene_geyser.closer_than(20, th)
+            vgs = self.vespene_geyser.closer_than(20, th)
 
         rp = list(self.main_base_ramp.corner_depots)
 
         self.buildOrder = [
             (17,Task(0, UnitTypeId.SUPPLYDEPOT, rp[1], None)),
             (29,Task(0, UnitTypeId.REFINERY, vgs[0], None)),
-            (44,Task(0, UnitTypeId.BARRACKS, self.main_base_ramp.barracks_in_middle, None)),
-            (92,Task(2, UnitTypeId.COMMANDCENTER, None, AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND)),
-            (94,Task(1, UnitTypeId.REAPER, UnitTypeId.BARRACKS, None))
+            (46,Task(0, UnitTypeId.BARRACKS, self.main_base_ramp.barracks_in_middle, None)),
+            (92,Task(2, UnitTypeId.COMMANDCENTER, UnitTypeId.ORBITALCOMMAND, AbilityId.UPGRADETOORBITAL_ORBITALCOMMAND)),
+            (94,Task(1, UnitTypeId.REAPER, UnitTypeId.BARRACKS, None)),
             # (103,Task(,,)),
             # (114,Task(,,)),
             # (128,Task(,,))
         ]
 
     async def on_step(self, iteration) :
-        await self.changeBuild()
+        # await self.changeBuild()
         self.sendTask()
-        await self.setArmyStance()
+        # await self.setArmyStance()
         await self.baseManager.doAction()
         # await self.armyLeader.doAction()
 
     def __init__(self):
+        self.unit_command_uses_self_do = True
         self.baseManager = BaseManager(self)
         # self.armyLeader = ArmyLeader(self)
